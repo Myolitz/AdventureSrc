2448cd2f84f029378f68e636c9306dacc944945c
kelak <yyounofree@gmail.com>
2023-05-24 00:45:39 -0500

    Alpha Version 0.1.2 - Front Entrance Partially Added

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
+/**
+ * Lazy Terminology Key:
+ * DNT = DO NOT TOUCH (aka its working as intended)
+ * NYI = Not yet implemented
+ * IT = In Testing
+ **/
+
 import PlayerData.PlayerData;
+import RoomData.*;
 import java.util.Scanner;
 
 public class Main {
@@ -9,57 +17,180 @@ public class Main {
         printMenu();
         int menuChoice = userChoice(input);
         boolean validChoice = false;
+        boolean gameStart = false;
+        String userStr = "";
 
-        //Ensures one of 3 choices, if not asks again
-        while (!validChoice) {
-            switch (menuChoice) {
-                case 1 -> { //The start of the game, create separate class later for levels
-                    System.out.println("Let's get started shall we then?");
-                    validChoice = true;
-                    String name = charName(input);
-                    PlayerData player = new PlayerData(name);
-
-                    //Test Lines below
-                    System.out.println("Your player name is: " + player.getName());
-                    player.getInventory();
-                }
-                case 2 -> { //Gives version, manually change this
-                    System.out.println("You are currently on version Alpha 0.1.1a");
-                    validChoice = true;
-                }
-                case 3 -> { //Quit
-                    System.out.println("Hope you'll come play again!");
-                    validChoice = true;
-                }
-                default -> {
-                    System.out.println("Oops, that's not an option!");
+        //Keeps you in the menu as long as you haven't selected the "play" option
+        while (!gameStart) {
+        //Ensures one of 4 choices, supports "Back" command
+            while (!validChoice || (userStr.equalsIgnoreCase("Back") || !(menuChoice == 4))) {
+                if (userStr.equalsIgnoreCase("Back")) {
                     printMenu();
                     menuChoice = userChoice(input);
+                    userStr = "";
+                }
+                switch (menuChoice) {
+                    case 1 -> { //The start of the game, create separate class later for levels
+                        System.out.println("Let's get started shall we then?");
+                        validChoice = true;
+                        gameStart = true;
+                        String name = charName(input);
+                        PlayerData player = new PlayerData(name);
+                        startGame(input, player);
+                        menuChoice = 4; //I have 0 idea why, but not having this makes it loop, so yeah. *IT*
+                    }
+                    case 2 -> { //Gives version. TODO: Manually change this
+                        System.out.println("You are currently on version Alpha 0.1.2");
+                        validChoice = true;
+                        userStr = userChoiceStr(input);
+                    }
+                    case 3 -> {
+                        //TODO: Update as you develop game
+                        System.out.println(
+                                """
+                                        There aren't many controls, through the basic ones include
+                                        - Forward
+                                        - Backward
+                                        - Inventory
+                                        - Interact
+                                        - Back, to back out of menus like this one
+                                        - Location
+                                        - You can type 'Quit' at any given type to exit the game once you start (NYI)
+                                        - Left/Right NYI"""
+                        );
+                        validChoice = true;
+                        userStr = userChoiceStr(input);
+                    }
+                    case 4 -> { //Quit
+                        System.out.println("Hope you'll come play again!");
+                        validChoice = true;
+                    }
+                    default -> {
+                        System.out.println("Oops, that's not an option!");
+                        menuChoice = userChoice(input);
+                        validChoice = false;
+                    }
                 }
             }
         }
-
-
-
         input.close();
     }
-    //Prints out menu using perfect size array
+
+    //Prints out menu using perfect size array - DNT
     public static void printMenu() {
-        String[] menuOptions = new String[] {"1. Play", "2. Version", "3. Quit"};
-        System.out.println("    Welcome to\n    " +
-                           "   The Adventure    ");
+        String[] menuOptions = new String[] {"1. Play", "2. Version", "3. Controls", "4. Quit"};
+        //noinspection SpellCheckingInspection
+        System.out.println("""
+                             Welcome to
+                           The Adventureᵀᴹ
+                           
+                         Created by me, Myo :)
+                """
+        );
         for (String menuOption : menuOptions) {
             System.out.println(menuOption);
         }
     }
 
+
     public static int userChoice(Scanner in) {
         return in.nextInt();
-    } //@see method name
+    }
+    //Yes I have to have 2 cause one returns number and the other String - DNT either
+    public static String userChoiceStr(Scanner in) {
+        return in.next();
+    }
 
-    public static String charName (Scanner in) { //Unsure if any dialogue will even happen, so method might get yeeted
+    //Unsure if any dialogue will even happen, so method might get yeeted - DNT (for now)
+    public static String charName (Scanner in) {
         System.out.println("Enter your characters name (Won't affect story that much).");
         return in.next();
     }
 
+    public static void startGame(Scanner in, PlayerData player) {
+
+        //Keeps the loop going ofc, only returning false and exiting once game is done
+        boolean gameRunning = true;
+        String playerLocation = player.getLocation();
+
+        //REMEMBER TO FUCKING UPDATE THE VARIABLE, YOU YES YOU FUTURE MYO
+        while (gameRunning) {
+            //Background, maybe replace with object elsewhere idk
+            System.out.println("""
+                            You had no idea where you were going after a night of drinking.
+                                    Was it even your house that you stumbled upon?
+                A feeling of uneasiness washes over, but with it raining you disregard the possible concerns.
+                   The door opens without so much as a speck of resistance and throw yourself into the house.
+                """);
+
+            switch (playerLocation) {
+                case "Front Entrance" -> {
+                    Entrance roomOne = new Entrance();
+                    frontEntrance(in, roomOne, player); //TODO: finish implementation, *IT*
+                    System.out.println("\nTest Room 1 Complete\n");
+                    gameRunning = false; //For test purposes, setting to false here, the logic in this entire switch statement might bite me in the ass later
+                    }
+                case "Room 2" -> {System.out.println("Test Room 2");} //TODO: implement
+                case "Room 3" -> {System.out.println("Test Room 3");} //TODO: implement
+                case "Room 4" -> {System.out.println("Test Room 4");} //TODO: implement
+                case "Room 5" -> {System.out.println("Test Room 5");} //TODO: implement
+                default -> {
+                    System.out.println("How did you even get this?");
+                    throw new ArrayIndexOutOfBoundsException(); //TODO: make this make sense by switching userpos to 2D array. or whatever francisco has recommended
+                }
+            }
+        }
+    }
+
+    public static void frontEntrance(Scanner in, Entrance roomOne, PlayerData player) {
+        //Commented to try out the object implementation
+//        System.out.println("""
+//                                    Almost as if you were transported to a victorian mansion,
+//                            you are hit with an immediate sense of luxury, are you meant to be here?
+//
+//                     Intricate patterns line the wall, opening up to a hallway straight head, past a potted plant
+//                and a coat rack that towers over you, leaving you wondering how one is supposed to hang their coat on it.
+//                """);
+
+        //Nested while loops lets fucking GOOOOOOOOOOOOOOOO
+        String choice = "";
+        System.out.println(roomOne.getRoomDesc());
+
+        //This entire thing is probably eternally IT, uh check necesssity of the commented code later
+        while (!(choice.equalsIgnoreCase("Forward"))) {
+            if (!(choice.equals(""))) {
+                System.out.println(roomOne.getRoomDesc());
+            }
+            choice = userChoiceStr(in);
+            switch (choice) {
+                //TODO: Inventory interactions
+                //TODO: Plant v Coat Rack Differentiation
+                case "Interact" -> {
+                    roomOne.interaction();
+//                    choice = userChoiceStr(in);
+                }
+                //TODO: Implement
+                case "Forward", "Left", "Right" -> {
+                    System.out.println("NYI");
+//                    choice = userChoiceStr(in);
+                }
+                case "Backward" -> {    //DNT
+                    System.out.println("It's raining outside, probably not a smart idea to go back outside.\n");
+//                    choice = userChoiceStr(in);
+                }
+                 case "Inventory" -> {  //DNT
+                    player.getInventory();
+//                    choice = userChoiceStr(in);
+                }
+                case "Quit" -> {break;}   //DNT
+                default -> {              //DNT
+                    System.out.println("Not a valid choice");
+//                    choice = userChoiceStr(in);
+                }
+            }
+            if (choice.equalsIgnoreCase("Quit")) { //DNT
+                break;
+            }
+        }
+    }
 }
