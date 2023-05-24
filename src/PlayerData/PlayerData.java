a3609014412e5ff0147f983a4b3dfed964131e94
kelak <yyounofree@gmail.com>
2023-05-24 00:46:51 -0500

    Updated to support possible OOB error

--- src/PlayerData/PlayerData.java
+++ src/PlayerData/PlayerData.java
@@ -5,10 +5,12 @@ import java.util.ArrayList;
 public class PlayerData {
     private String name;
     private ArrayList<String> inventory;
+    private String location;
 
     public void defaultValues() {
         name = "none";
         inventory = new ArrayList<String>(); //Prohibits NullPointerException, ran by PlayerData.PlayerData
+        location = "Front Entrance";
     }
 
     public PlayerData(String name) { //Constructor/Setter, calls upon defaultValues
@@ -19,9 +21,10 @@ public class PlayerData {
     public String getName() { //Simple Getter
         return name;
     }
+
     public void getInventory() { //Checks player inventory, accessible via commands
         if (inventory.isEmpty()) {
-            System.out.println("You got some picket lint...that's about it");
+            System.out.println("You got some picket lint...that's about it\n");
         }
         else {
             for (int i = 0; i < inventory.size(); i++) {
@@ -29,4 +32,16 @@ public class PlayerData {
             }
         }
     }
+    public String getLocation() {
+        return location;
+    } //TODO: Francisco shenanigans, go bug him :troll:
+
+    public void setLocation(String newLocation) {
+        this.location = newLocation;
+    }
+
+    public String outOfBounds() { //DNT, failsafe of sorts
+        return "How did you get here?";
+    }
+
 }
